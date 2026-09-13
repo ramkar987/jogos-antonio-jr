90f2201a15a23cbcaec18c2e8bc74b8eff5533bd
ramkar987 <credicard@gmail.com>
2026-09-13 20:34:20 -0300

    chore: atualizar cache offline para o álbum

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mundo-antonio-jr-v1';
-const CORE=['./','./index.html','./style.css','./app.js','./games.json','./jogar.html','./manifest.webmanifest','./icon.svg','./jogos/novos-jogos.html'];
+const CACHE='mundo-antonio-jr-v2';
+const CORE=['./','./index.html','./style.css','./album.css','./app.js','./album.js','./games.json','./jogar.html','./manifest.webmanifest','./icon.svg','./jogos/novos-jogos.html'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
